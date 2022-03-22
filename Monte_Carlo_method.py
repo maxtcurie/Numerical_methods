82d2617d3b03f3b5e5a5f348995c62a7db3a8903
maxcurie1996 <maxcurie@utexas.edu>
2022-03-22 12:28:59 -0700

    MC basic

--- Monte_Carlo_method.py
+++ Monte_Carlo_method.py
@@ -1,5 +1,7 @@
 import numpy as np
-import pandas as pd
+#import pandas as pd
+import matplotlib.pyplot as plt
+
 
 n=1000
 x_list=np.random.rand(n)
@@ -13,3 +15,13 @@ for x,y in zip(x_list, y_list):
 pi=4.*float(count)/float(n)
 
 print('pi='+str(pi))
+
+
+theta=np.arange(0,7,0.001)
+
+plt.figure(figsize=(5,5),dpi=96) 
+plt.scatter(x_list,y_list,alpha=0.2)
+plt.plot(np.cos(theta),np.sin(theta))
+plt.xlim(0,1.2)
+plt.ylim(0,1.2)
+plt.show()
